c33f63925c4cad2fd4d6b3d04a2e536ae3c8fb6a
MoJoVi <kezikov.maxim@gmail.com>
2019-10-07 22:11:59 +0500

    Minor changes

--- week3/cars.py
+++ week3/cars.py
@@ -1,55 +1,7 @@
 """
 Задача:
-Создать иерархию классов для сортировки данных об автомобилях.
-У любого объекта есть обязательный атрибут car_type.
-Он означает тип объекта и может принимать одно из значений:
-car, truck, spec_machine.
-
-Также у любого объекта из иерархии есть фото в виде имени файла
-— обязательный атрибут photo_file_name.
-
-В базовом классе нужно реализовать метод get_photo_file_ext
-для получения расширения файла (“.png”, “.jpeg” и т.д.) с фото.
-Расширение файла можно получить при помощи os.path.splitext.
-
-Для грузового автомобиля необходимо разделить характеристики кузова
-на отдельные составляющие body_length, body_width, body_height.
-Разделитель — латинская буква x. Характеристики кузова могут
-быть заданы в виде пустой строки, в таком случае все составляющие
-равны 0. Обратите внимание на то, что характеристики кузова должны быть
-вещественными числами.
-
-Также для класса грузового автомобиля необходимо реализовать метод
-get_body_volume, возвращающий объем кузова в метрах кубических.
-
-Все обязательные атрибуты для объектов Car, Truck и SpecMachine
-перечислены в таблице ниже, где 1 - означает, что атрибут
-обязателен для объекта, 0 - атрибут должен отсутствовать.
-
-Атрибуты \ Типы машин    Car	    Truck	   SpecMachine
-car_type	              1	          1	           1
-photo_file_name	          1           1	           1
-brand	                  1	          1	           1
-carrying	              1           1	           1
-passenger_seats_count	  1	          0	           0
-body_width	              0	          1	           0
-body_height	              0	          1	           0
-body_length	              0	          1	           0
-extra	                  0	          0	           1
-
-Далее необходимо реализовать функцию, на вход которой подается
-имя файла в формате csv. Файл содержит данные аналогичные строкам
-из таблицы. Вам необходимо прочитать этот файл построчно при помощи
-модуля стандартной библиотеки csv. Затем проанализировать строки и
-создать список нужных объектов с автомобилями и специальной техникой.
-Функция должна возвращать список объектов.
-
-У каждого объекта из иерархии должен быть свой набор атрибутов и методов.
-У класса легковой автомобиль не должно быть метода get_body_volume в
-отличие от класса грузового автомобиля.
-
-Функция, которая парсит строки входного массива, должна
-называться get_car_list.
+Создать иерархию классов для сортировки и удобочитаемого отображения
+данных об автомобилях.
 """
 
 
@@ -58,12 +10,24 @@ import csv
 
 
 class CarBase:
+    """
+    Базовый класс
+    """
     def __init__(self, brand, photo_file_name, carrying):
+        """
+        Присваеваем базовые характеристики, которые есть у всех авто
+        :param brand: Фирма авто.
+        :param photo_file_name: имя файла с фото авто
+        :param carrying: Грузободъемность
+        """
         self.brand = brand
         self.photo_file_name = photo_file_name
         self.carrying = carrying
 
     def get_photo_file_ext(self):
+        """
+        :return: разрешение файла с фото авто
+        """
         return os.path.splitext(self.photo_file_name)[-1]
 
     def __str__(self):
@@ -82,6 +46,9 @@ class Car(CarBase):
     car_type = 'car'
 
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+        """
+        :param passenger_seats_count: Количество мест.
+        """
         super().__init__(brand, photo_file_name, carrying)
         self.passenger_seats_count = int(passenger_seats_count)
 
@@ -90,6 +57,9 @@ class Truck(CarBase):
     car_type = 'truck'
 
     def __init__(self, brand, photo_file_name, carrying, body_whl):
+        """
+        :param body_whl: Характеристики кузова.
+        """
         super().__init__(brand, photo_file_name, carrying)
 
         if body_whl:
@@ -106,21 +76,34 @@ class SpecMachine(CarBase):
     car_type = 'spec_machine'
 
     def __init__(self, brand, photo_file_name, carrying, extra):
+        """
+        :param extra: Назначение авто.
+        """
         super().__init__(brand, photo_file_name, carrying)
         self.extra = extra
 
 
 def sorting_args(args, ix):
+    """
+    :param args: агрументы
+    :param ix: Индекс специального аргумента
+    :return: список аргументов для данного вида авто
+    """
     return [args[0], args[2], float(args[4]), args[ix]]
 
 
 def get_car_list(csv_filename):
+    """
+    :param csv_filename: csv-файл из которого необходимо взять параметры авто
+    :return: словарь с объектами классов автомобилей
+    """
     cars = {
         'car': [1, Car],
         'truck': [3, Truck],
         'spec_machine': [5, SpecMachine]
-    }
-    car_list = []
+    }  # Словарь создан для сортировки авто и вызова необходимых классов и
+    # чтобы избежать большого количества if-ов
+    car_list = []  # список с итоговым результатом
 
     try:
         with open(csv_filename, encoding='utf8') as csv_fd:
