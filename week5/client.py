aee4e378169417a62b4388476b874b4ee1637bb9
MoJoVi <kezikov.maxim@gmail.com>
2019-10-07 20:52:59 +0500

    updated

--- week5/client.py
+++ week5/client.py
@@ -1,103 +1,102 @@
+"""
+Задача:
+написать сетевую программу - клиент, которая будет уметь отправлять данные
+различных метрик на сервер, запрашивать информацию хранящихся на сервере
+и обрабатывать его ответы.
+"""
 import socket
 import time
 
 
-class ClientError(Exception):
-    """Общий класс исключений клиента"""
-    pass
-
-
-class ClientSocketError(ClientError):
-    """Исключение, выбрасываемое клиентом при сетевой ошибке"""
-    pass
-
-
-class ClientProtocolError(ClientError):
-    """Исключение, выбрасываемое клиентом при ошибке протокола"""
+class ClientError(socket.error):
+    """Пользовательский класс исключений."""
     pass
 
 
 class Client:
     def __init__(self, host, port, timeout=None):
-        # класс инкапсулирует создание сокета
-        # создаем клиентский сокет, запоминаем объект socke.socket в self
-        self.host = host
-        self.port = port
         try:
-            self.connection = socket.create_connection((host, port), timeout)
+            self.sock = socket.create_connection((host, port), timeout)
         except socket.error as err:
-            raise ClientSocketError("error create connection", err)
+            self._save_log_and_raise_err(err, "Error create connection")
 
-    def _read(self):
-        """Метод для чтения ответа сервера"""
-        data = b""
-        # накапливаем буфер, пока не встретим "\n\n" в конце команды
-        while not data.endswith(b"\n\n"):
+    def _push(self, data):
+        """
+        внутренний метод для отправуи данных на сервер, при ошибке
+        отправки данных - выбрасывает пользовательское исключение.
+        :param data: данные которые необходимо отправить.
+        :return: None.
+        """
+        try:
+            self.sock.sendall(data)
+        except socket.error as err:
+            self._save_log_and_raise_err(err, 'Error sending data to server')
+
+    def _save_log_and_raise_err(self, error, description):
+        """
+        Внутренний метод для записи ошибок в лог файл, после чего
+        вызывает клиентский класс исключений с заданным текстом описания
+        ошибки.
+        :param error: сама ошибка для сохранения в лог файл.
+        :param description: описание ошибки для пользователя.
+        :return: None
+        """
+        with open('client_log', 'a') as log_file:
+            log_file.write(str(error))
+            raise ClientError(description)
+
+    def _server_ans(self):
+        """
+        Внутренний метод считывающий ответ сервера.
+        :return: Данные полученные с сервера.
+        """
+        data = ''
+        while data[-2:] != '\n\n':
             try:
-                data += self.connection.recv(1024)
-            except socket.error as err:
-                raise ClientSocketError("error recv data", err)
-
-        # не забываем преобразовывать байты в объекты str для дальнейшей работы
-        decoded_data = data.decode()
-
-        status, payload = decoded_data.split("\n", 1)
-        payload = payload.strip()
-
-        # если получили ошибку - бросаем исключение ClientError
-        if status == "error":
-            raise ClientProtocolError(payload)
-
-        return payload
-
-    def put(self, key, value, timestamp=None):
-        timestamp = timestamp or int(time.time())
-
-        # отправляем запрос команды put
-        try:
-            self.connection.sendall(
-                f"put {key} {value} {timestamp}\n".encode()
-            )
-        except socket.error as err:
-            raise ClientSocketError("error send data", err)
-
-        # разбираем ответ
-        self._read()
-
-    def get(self, key):
-        # формируем и отправляем запрос команды get
-        try:
-            self.connection.sendall(
-                f"get {key}\n".encode()
-            )
-        except socket.error as err:
-            raise ClientSocketError("error send data", err)
-
-        # читаем ответ
-        payload = self._read()
-
-        data = {}
-        if payload == "":
-            return data
-
-        # разбираем ответ для команды get
-        for row in payload.split("\n"):
-            key, value, timestamp = row.split()
-            if key not in data:
-                data[key] = []
-            data[key].append((int(timestamp), float(value)))
-
+                data += self.sock.recv(1024).decode()
+            except socket.error as er:
+                self._save_log_and_raise_err(er, 'Error receiving response from server')
+        if data.startswith('err'):
+            raise ClientError('Wrong command')
         return data
 
-    def close(self):
-        try:
-            self.connection.close()
-        except socket.error as err:
-            raise ClientSocketError("error close connection", err)
+    def put(self, key, value, timestamp=time.time()):
+        """
+        Метод для отправки данных на сервер.
+        :param key: наименование метрики.
+        :param value: значение данной метрики.
+        :param timestamp: время замера.
+        :return: None.
+        """
+        self._push(f'put {key} {value} {timestamp}\n'.encode())
+        self._server_ans()
+
+    def get(self, key):
+        """
+        метод для запроса данных с сервера, при необходимости запроса
+        всех данных используется символ "*"
+        :param key: наименование метрики данные по которой необходимо получить
+        :return: словарь со всеми значениями метрики, которые хранятся на сервере
+        """
+        self._push(f'get {key}\n'.encode())
+        data = self._server_ans().split('\n')
+        data = [line for line in data[1:] if line]
+        res_dict = {}
+        for metric in data:
+            key, value, timestamp = metric.split()
+            res_dict[key] = res_dict.get(key, list()) + [(int(timestamp), float(value))]
+        return res_dict
+
+    def __del__(self):
+        """
+        Перегруженный метод закрывает соединение при закрытии программы
+        или удалении объекта класса
+        :return: None
+        """
+        self.sock.close()
 
 
-def _main():
-    # проверка работы клиента
+if __name__ == '__main__':
     client = Client("127.0.0.1", 8888, timeout=5)
     client.put("test", 0.5, timestamp=1)
     client.put("test", 2.0, timestamp=2)
@@ -105,9 +104,3 @@ def _main():
     client.put("load", 3, timestamp=4)
     client.put("load", 4, timestamp=5)
     print(client.get("*"))
-
-    client.close()
-
-
-if __name__ == "__main__":
-    _main()
